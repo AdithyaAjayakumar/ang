7c6025f7cbbbde38261fe64bddadd2e19ffa34d7
AdithyaAjayakumar <Adithya2000p@gmail.com>
2022-06-01 22:36:08 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { ChildComponent } from './child/child.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+@ViewChild(ChildComponent)viewdata !:ChildComponent;
+title = 'new-project';
+inputname='';
+inputmark='';
+inputobj={"name":"","mark":""};
+TransferData(name:any,mark:any){
+  this.inputname=name;
+  this.inputmark=mark;
+  this.inputobj={"name":name,"mark":mark};
+  this.viewdata.updatelist(this.inputobj);
+
+}
+ 
+}
+
+
+
+
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input() namedata:any;
+  @Input() markdata:any;
+  @Input() objdata:any;
+  listarray =[{"name":"","mark":""}]
+  constructor() { 
+
+  }
+  updatelist(obj:any){
+   this.listarray.push(obj);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
